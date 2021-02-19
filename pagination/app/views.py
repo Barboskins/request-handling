06055007961e27aa039f99b7484a72e89f93f1a9
Nikita Kondrashov <nickita.kondraschov@yandex.ru>
2021-02-19 21:13:10 +0300

    pagination commit

---
+++ pagination/app/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.conf import settings
+import csv
+from django.core.paginator import Paginator
+
+
+def index(request):
+    return redirect(reverse(bus_stations))
+
+
+def bus_stations(request):
+    with open (settings.BUS_STATION_CSV) as f:
+        f_read = csv.DictReader(f)
+        paginator = Paginator(list(f_read), settings.PAG_BUS_STATION_PER_PAGE)
+        page_number = int(request.GET.get('page',1))
+        current_page = paginator.get_page(page_number)
+        msg = current_page.object_list
+        if current_page.has_next():
+            next_page_url = current_page.next_page_number()
+    return render(request, 'index.html', context={
+        'bus_stations': msg,
+        'current_page': current_page,
+        'prev_page_url': None,
+        'next_page_url': next_page_url,
+    })
+
+
